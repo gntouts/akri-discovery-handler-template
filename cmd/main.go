682b2d4a4e95fba18ca597a0c9304d37736cf934
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2024-10-23 17:17:13 +0300

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
+const (
+	agentRegistrationSocketName string = "agent-registration.sock"
+	discoveryHandlerPrefix      string = "akri-discovery-handler-template"
+)
+
+var (
+	agentRegistrationSocketPath string
+	discoveryHandlerName        string
+	discoveryServiceSocketPath  string
+)
+
+func init() {
+	envVar := os.Getenv("DISCOVERY_HANDLER_SUFFIX")
+	discoveryHandlerName = fmt.Sprintf("%s%s", discoveryHandlerPrefix, envVar)
+
+	envVar = os.Getenv("DISCOVERY_HANDLERS_DIRECTORY")
+	if envVar == "" {
+		envVar = "/var/lib/akri"
+	}
+	agentRegistrationSocketPath = filepath.Join(envVar, agentRegistrationSocketName)
+	discoveryServiceSocketPath = filepath.Join(envVar, fmt.Sprintf("%s.sock", discoveryHandlerName))
+	os.RemoveAll(discoveryServiceSocketPath)
+}
+
 func main() {
-	fmt.Println("go-http-range started")
+	fmt.Println("akri-discovery-handler-template started")
 }
